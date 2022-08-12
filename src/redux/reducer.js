cf692047dee9bcb3bfaee24876bd90ffed8c6327
Kateryna Borysenko <k.borysenko.kyiv@gmail.com>
2022-08-12 12:45:10 +0300

    First commit

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  name: '',
+  todos: []
+};
 
 const addTodoReducer = createSlice({
   name: "todos",
